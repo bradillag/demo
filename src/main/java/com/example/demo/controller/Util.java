dddb7cbe402a69fb4f42a2b2ca919c1c56fc55da
bradilla <bradilla@METPPBRADILLA.izzitelecom.net>
2020-10-06 10:49:54 -0500

    Demo project

---
+++ src/main/java/com/example/demo/controller/Util.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+
+public class Util {
+	
+//	public static void main(String[] args) {  
+//	    SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyyHHmmss");  
+//	    
+//	    System.out.println(formatter.format(new Date()));  
+//	}  
+
+}
